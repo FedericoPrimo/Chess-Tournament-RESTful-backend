d3d318753088ffba365f57b480a6a31df57e9cc1
FedericoPrimo <federico.calz02@gmail.com>
2025-01-29 13:19:33 +0100

    Controller update

--- src/main/java/it/unipi/enPassant/controller/SpectatorController.java
+++ src/main/java/it/unipi/enPassant/controller/SpectatorController.java
@@ -1,25 +1,39 @@
 package it.unipi.enPassant.controller;
 
+import it.unipi.enPassant.model.requests.LoginRequest;
 import it.unipi.enPassant.service.AuthenticationService;
 import it.unipi.enPassant.service.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/spectator")
-public class SpectatorController {
-  // Services used for authentication and interacting with the databases.
-  private final AuthenticationService spectatorservice;
-  private final DataService spectatorDataService;
+public class SpectatorController extends GenericUserController{
 
   // Constructor for the SpectatorController
   @Autowired
-  public SpectatorController(AuthenticationService spectatorservice, DataService spectatorDataService){
-    this.spectatorservice = spectatorservice;
-    this.spectatorDataService = spectatorDataService;
+  public SpectatorController(AuthenticationService authservice, DataService dataservice){
+    super(authservice, dataservice);
   }
 
   /* Here we have all the API endpoints */
+  @PostMapping("/login")
+  public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+    try {
+      if(authservice.PlayerLoginControl(loginRequest)){
+        return new ResponseEntity<>("Login successful", HttpStatus.OK);
+      }
+      else{
+        return new ResponseEntity<>("Login failed", HttpStatus.UNAUTHORIZED);
+      }
 
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
