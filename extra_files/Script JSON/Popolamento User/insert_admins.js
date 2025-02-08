2643d2240c47740e66c332bb368c8c5b2498f957
FedericoPrimo <federico.calz02@gmail.com>
2025-02-08 15:39:15 +0100

    Updated scripts for mongo with hashed psw

--- extra_files/Script JSON/Popolamento User/insert_admins.js
+++ extra_files/Script JSON/Popolamento User/insert_admins.js
@@ -1,26 +1,32 @@
-db.user.insertMany([
-    {'_id': 'gojo_satoru',
-     'Name': 'Satoru',
-     'Surname': 'Gojo',
-     'BirthDate': '1989-12-7',
-     'Password': 'gojo_satoru',
-     'Type': '0'},
-    {'_id': 'sung_jinwoo',
-     'Name': 'Jinwoo',
-     'Surname': 'Sung',
-     'BirthDate': '2001-03-8',
-     'Password': 'sung_jinwoo',
-     'Type': '0'},
-    {'_id': 'mustang_roy',
-     'Name': 'Roy',
-     'Surname': 'Mustang',
-     'BirthDate': '1975-03-25',
-     'Password': 'mustang_roy',
-     'Type': '0'},
-     {'_id': 'uchiha_itachi',
-      'Name': 'Itachi',
-      'Surname': 'Uchiha',
-      'BirthDate': '1985-06-9',
-      'Password': 'uchiha_itachi',
-      'Type': '0'}
-    ]);
+db.user.insertMany([{
+	  "_id": "gojo_satoru",
+	  "BirthDate": "1989-12-7",
+	  "Name": "Satoru",
+	  "Password": "$2a$10$Nfg/icoEfFQifTZdTm.hr.NsuIpI1O/.E5ZAhaiPk67ExPL9B/xDq",
+	  "Surname": "Gojo",
+	  "Type": "0"
+	},
+	{
+	  "_id": "sung_jinwoo",
+	  "BirthDate": "2001-03-8",
+	  "Name": "Jinwoo",
+	  "Password": "$2a$10$oeYK1ftH3gyS6h4ZJLN1.exIur5LFgibE6R3WaBcVPix7t8z8gg86",
+	  "Surname": "Sung",
+	  "Type": "0"
+	},
+	{
+	  "_id": "mustang_roy",
+	  "BirthDate": "1975-03-25",
+	  "Name": "Roy",
+	  "Password": "$2a$10$UjKu3FTl4Q0GibTrf3wjx.SN/7xaBr3vfUVJYhgkgYfAHT/OCzqV6",
+	  "Surname": "Mustang",
+	  "Type": "0"
+	},
+	{
+	  "_id": "uchiha_itachi",
+	  "BirthDate": "1985-06-9",
+	  "Name": "Itachi",
+	  "Password": "$2a$10$i9PemlJcjBkxWD6YGif.eOp2BexgIZwHfa2DENrSJ.hQpYNFjFJG.",
+	  "Surname": "Uchiha",
+	  "Type": "0"
+	}]);
